97e1c4f9c82b31192c159e438ed221aa8eb48ddc
Garnet3106 <manage@gant.work>
2020-02-18 15:24:52 +0900

    Format code style of bytecode.cpp

--- dev/src/bytecode.cpp
+++ dev/src/bytecode.cpp
@@ -22,80 +22,83 @@ FuncData_::FuncData_(std::vector<unsigned char> i, std::vector<unsigned char> nm
 
 Bytecode::Bytecode() {}
 
-Bytecode::Bytecode(std::vector<unsigned char> src) {
-    source = src;
+Bytecode::Bytecode(std::vector<unsigned char> source) {
+    this->source = source;
 }
 
-Bytecode::Bytecode(int src) { //
-    source.push_back((unsigned char)src);
+Bytecode::Bytecode(int source) {
+    // todo: 256以上の数字にも対応させる
+    this->source.push_back((unsigned char)source);
 }
 
-Bytecode::Bytecode(std::string src) {
-    for(unsigned char s : src)
-        source.push_back(s);
+Bytecode::Bytecode(std::string source) {
+    for(unsigned char src : source)
+        this->source.push_back(src);
 }
 
 Bytecode::Bytecode(Node tree) {
-    source = toBytecode(tree).source;
+    this->source = this->toBytecode(tree).source;
 }
 
-Bytecode::Bytecode(std::vector<Bytecode> src) {
-    for(Bytecode bc : src) {
+Bytecode::Bytecode(std::vector<Bytecode> source) {
+    for(Bytecode bc : source) {
         append(Bytecode("compiled_ches"));
         append(Bytecode(std::vector<unsigned char> { SI_LNDIV, SI_BCSTART, SI_LNDIV }));
         append(Bytecode(bc));
     }
 }
 
-Bytecode::Bytecode(std::vector<std::vector<std::vector<unsigned char>>> src) {
-    for(int ln = 0; ln < src.size(); ln++) {
-        for(int tk = 0; tk < src[ln].size(); tk++) {
-            for(int ch = 0; ch < src[ln][tk].size(); ch++) {
-                if(src[ln][tk][ch] == SI_LNDIV) append(SI_LNDIV);
-                else if(src[ln][tk][ch] == SI_TKDIV) append(SI_TKDIV);
-                append(src[ln][tk][ch]);
+Bytecode::Bytecode(std::vector<std::vector<std::vector<unsigned char>>> source) {
+    for(int ln = 0; ln < source.size(); ln++) {
+        for(int tk = 0; tk < source[ln].size(); tk++) {
+            for(int ch = 0; ch < source[ln][tk].size(); ch++) {
+                if(source[ln][tk][ch] == SI_LNDIV) this->append(SI_LNDIV);
+                else if(source[ln][tk][ch] == SI_TKDIV) this->append(SI_TKDIV);
+                this->append(source[ln][tk][ch]);
             }
-            if(tk != src[ln].size() - 1) append(SI_TKDIV);
+
+            if(tk != source[ln].size() - 1) this->append(SI_TKDIV);
         }
-        if(ln != src.size() - 1) append(SI_LNDIV);
+
+        if(ln != source.size() - 1) this->append(SI_LNDIV);
     }
 }
 
-Bytecode Bytecode::append(unsigned char src) {
-    source.push_back(src);
-    return source;
+Bytecode Bytecode::append(unsigned char source) {
+    this->source.push_back(source);
+    return this->source;
 }
 
-Bytecode Bytecode::append(Bytecode src) {
-    for(unsigned char uc : src.source)
-        source.push_back(uc);
-    return source;
+Bytecode Bytecode::append(Bytecode source) {
+    for(unsigned char srcChar : source.source)
+        this->source.push_back(srcChar);
+    return this->source;
 }
 
 Bytecode Bytecode::pop() {
-    source.pop_back();
-    return source;
+    this->source.pop_back();
+    return this->source;
 }
 
 int Bytecode::toInt() {
-    return *(unsigned int *)&source;
+    return *(unsigned int *)&this->source;
 }
 
 std::string Bytecode::toString() {
     std::string res;
-    for(unsigned char c : source)
-        res.push_back(c);
+    for(unsigned char srcChar : this->source)
+        res.push_back(srcChar);
     return res;
 }
 
 // vector型の命令を取得
 std::vector<std::vector<std::vector<unsigned char>>> Bytecode::divide() {
-    std::vector<std::vector<std::vector<unsigned char>>> res = {{{}}};
+    std::vector<std::vector<std::vector<unsigned char>>> res;
 
     for(int i = 0; i < source.size(); i++) {
-        switch(source[i]) {
+        switch(this->source[i]) {
             case SI_LNDIV:
-            if(i + 1 < source.size() && source[i + 1] == SI_LNDIV) {
+            if(i + 1 < this->source.size() && this->source[i + 1] == SI_LNDIV) {
                 res.back().back().push_back(SI_LNDIV);
                 i++;
             } else {
@@ -104,7 +107,7 @@ std::vector<std::vector<std::vector<unsigned char>>> Bytecode::divide() {
             break;
 
             case SI_TKDIV:
-            if(i + 1 < source.size() && source[i + 1] == SI_TKDIV) {
+            if(i + 1 < this->source.size() && this->source[i + 1] == SI_TKDIV) {
                 res.back().back().push_back(SI_TKDIV);
                 i++;
             } else {
@@ -113,7 +116,7 @@ std::vector<std::vector<std::vector<unsigned char>>> Bytecode::divide() {
             break;
 
             default:
-            res.back().back().push_back(source[i]);
+            res.back().back().push_back(this->source[i]);
             break;
         }
     }
@@ -122,41 +125,39 @@ std::vector<std::vector<std::vector<unsigned char>>> Bytecode::divide() {
 }
 
 Bytecode Bytecode::toBytecode(Node tree) {
-    lines.push_back({ { 0x63, 0x6f, 0x6d, 0x70, 0x69, 0x6c, 0x65, 0x64, 0x5f, 0x63, 0x68, 0x65, 0x73 } });
+    this->lines.push_back({ { 0x63, 0x6f, 0x6d, 0x70, 0x69, 0x6c, 0x65, 0x64, 0x5f, 0x63, 0x68, 0x65, 0x73 } });
 
-    for(Node nd : tree.children) {
-        if(nd.type == N_DEFFUNC) {
-            funcdata.push_back(FuncData(Bytecode(getRandom(10)).source, Bytecode(nd.tokenAt(0).string).source));
-        }
-    }
+    for(Node node : tree.children)
+        if(node.type == N_DEFFUNC)
+            funcdata.push_back(FuncData(Bytecode(this->generateUUID()).source, Bytecode(node.tokenAt(0).string).source));
 
     while(index < tree.children.size()) {
-        toBytecode_scan(tree.childAt(index));
+        nodeToBytecode(tree.childAt(index));
         index++;
     }
 
     return Bytecode(lines);
 }
 
-void Bytecode::toBytecode_scan(Node node) {
+void Bytecode::nodeToBytecode(Node node) {
     try {
 
         switch(node.type) {
             case N_CALLFUNC: {
                 std::vector<unsigned char> funcname = Bytecode(node.tokenAt(0).string).source;
-                lines.push_back({ { I_CALL }, FuncData::findByName(funcdata, funcname).id });
+                this->lines.push_back({ { I_CALL }, FuncData::findByName(funcdata, funcname).id });
             } break;
 
             case N_DEFFUNC: {
                 std::vector<unsigned char> funcname = Bytecode(node.tokenAt(0).string).source;
                 std::vector<unsigned char> funcid = Bytecode(FuncData::findByName(funcdata, funcname).id).source;
-                lines.push_back({ { I_GROUP }, funcid, funcname });
+                this->lines.push_back({ { I_GROUP }, funcid, funcname });
                 if(node.children.size() > 1)
                     lllen += node.childAt(0).children.size();
             } break;
 
             case N_DEFVAR: {
-                lines.push_back({ { I_LSPUSH }, {} });
+                this->lines.push_back({ { I_LSPUSH }, {} });
                 lslen++;
             } break;
 
@@ -174,7 +175,7 @@ void Bytecode::toBytecode_scan(Node node) {
                     value = Bytecode(std::stoi(token.string)).source;
                 else if(token.type == STRING)
                     value = Bytecode(token.string).source;
-                lines.push_back({ { I_LSPUSH }, value });
+                this->lines.push_back({ { I_LSPUSH }, value });
                 lslen++;
             } break;
         }
@@ -184,10 +185,8 @@ void Bytecode::toBytecode_scan(Node node) {
     }
 }
 
-std::vector<unsigned char> Bytecode::getRandom(int num) {
-    std::vector<unsigned char> res;
-    std::random_device rnd;
-    for(int i = 0; i < num; i++)
-        res.push_back(rnd() % std::numeric_limits<unsigned char>::max());
-    return res;
+unsigned char Bytecode::generateUUID() {
+    uuid_t uuid;
+    uuid_generate(uuid);
+    return *uuid;
 }
