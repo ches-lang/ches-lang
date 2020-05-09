8c4edc9e2eb6615caa149840c5ee8c9a08e14aa7
Garnet3106 <manage@gant.work>
2020-05-10 05:46:15 +0900

    Arrange FileManager class

--- dev/src/filemanager.hpp
+++ dev/src/filemanager.hpp
@@ -6,28 +6,85 @@ class FileManager {
 
 public:
 
+    static bool exists(std::string path) {
+        return std::filesystem::exists(path);
+    }
+
+    static std::string getFileExt(std::string path) {
+        std::string ext;
+
+        for(int i = path.length() - 1; i >= 0; i--) {
+            if(path[i] == '.') break;
+            else ext = path[i] + ext;
+        }
+
+        return ext;
+    }
+
+    static std::string getFileName(std::string path) {
+        std::filesystem::path convedPath = path;
+        return convedPath.filename();
+    }
+
+    static std::vector<std::string> getFilePaths(std::string dirPath) {
+        std::vector<std::string> filePaths;
+
+        try {
+            std::filesystem::directory_iterator itr(dirPath), end;
+            std::error_code err;
+
+            if(!FileManager::exists(dirPath))
+                Console::log(LogType_Error, "0327", { { "Path", dirPath } }, true);
+
+            for(; itr != end && !err; itr.increment(err)) {
+                const std::filesystem::directory_entry entry = *itr;
+                filePaths.push_back(entry.path().string());
+            }
+
+            if(err)
+                Console::log(LogType_Error, "6845", { { "Directory", dirPath }, { "message", err.message() }}, true);
+
+        } catch(std::filesystem::filesystem_error fse) {
+            Console::log(LogType_Error, "6845", { { "Directory", dirPath } }, true);
+        }
+
+        return filePaths;
+    }
+
+    static std::string getFullPath(std::string path) {
+        return std::filesystem::absolute(path);
+    }
+
+    static bool isDirectory(std::string path) {
+        return std::filesystem::is_directory(path);
+    }
+
     static Bytecode readBytecode(std::string path) {
         try {
-            Bytecode res;
+            Bytecode fileCont;
 
             std::ifstream ifs(path);
+
             if(!ifs.is_open())
                 Console::log(LogType_Error, "0327", { { "Path", path } }, true);
+
             if(ifs.fail())
                 Console::log(LogType_Error, "6845", { { "Path", path } }, true);
 
             unsigned char uc;
+
             do {
                 ifs.read((char*)&uc, sizeof(unsigned char));
-                res.source.push_back(uc);
+                fileCont.source.push_back(uc);
             } while(!ifs.eof());
-            res.source.pop_back();
 
+            fileCont.source.pop_back();
             ifs.close();
-            return res;
+            return fileCont;
         } catch(std::exception excep) {
             Console::log(LogType_Error, "6845", { { "Path", path } }, true);
         }
+
         return Bytecode();
     }
 
@@ -41,10 +98,10 @@ public:
             if(ifs.fail())
                 Console::log(LogType_Error, "6845", { { "Path", path } }, true);
 
-            std::string res = std::string((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
+            std::string fileCont = std::string((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
 
             ifs.close();
-            return res;
+            return fileCont;
         } catch(std::exception excep) {
             Console::log(LogType_Error, "6845", { { "Path", path } }, true);
         }
@@ -62,14 +119,14 @@ public:
             if(ifs.fail())
                 Console::log(LogType_Error, "6845", { { "Path", path } }, true);
 
-            std::vector<std::string> res;
+            std::vector<std::string> fileCont;
             std::string line;
 
             while(std::getline(ifs, line))
-                res.push_back(line);
+                fileCont.push_back(line);
 
             ifs.close();
-            return res;
+            return fileCont;
         } catch(std::exception excep) {
             Console::log(LogType_Error, "6845", { { "Path", path } }, true);
         }
@@ -77,12 +134,25 @@ public:
         return {};
     }
 
+    static std::string renamePathExt(std::string path, std::string newExt) {
+        std::string newPath;
+        std::string oldExt = FileManager::getFileExt(path);
+
+        if(path == newExt) {
+            newPath = path + "." + oldExt;
+        } else {
+            newPath = path.substr(0, path.length() - oldExt.length()) + newExt;
+        }
+
+        return newPath;
+    }
+
     static void writeBytecode(std::string path, Bytecode src) {
         try {
             std::ofstream ofs;
             ofs.open(path, std::ios::out | std::ios::binary | std::ios::trunc);
 
-            if (!ofs)
+            if(!ofs)
                 Console::log(LogType_Error, "5352", { { "Path", path } }, true);
 
             for(unsigned char uc : src.source)
@@ -92,65 +162,5 @@ public:
         } catch(std::exception excep) {
             Console::log(LogType_Error, "5352", { { "Path", path } }, true);
         }
-    }
-
-    static bool exists(std::string path) {
-        return std::filesystem::exists(path);
-    }
-
-    static bool isDirectory(std::string path) {
-        return std::filesystem::is_directory(path);
-    }
-
-    static std::vector<std::string> getFilePaths(std::string dirpath) {
-        std::vector<std::string> filepaths;
-        try {
-            std::filesystem::directory_iterator iter(dirpath), end;
-            std::error_code err;
-
-            if(!FileManager::exists(dirpath))
-                Console::log(LogType_Error, "0327", { { "Path", dirpath } }, true);
-
-            for(; iter != end && !err; iter.increment(err)) {
-                const std::filesystem::directory_entry entry = *iter;
-                filepaths.push_back(entry.path().string());
-            }
-
-            if(err)
-                Console::log(LogType_Error, "6845", { { "Directory", dirpath }, { "message", err.message() }}, true);
-
-        } catch(std::filesystem::filesystem_error fse) {
-            Console::log(LogType_Error, "6845", { { "Directory", dirpath } }, true);
-        }
-
-        return filepaths;
-    }
-
-    static std::string getFileName(std::string path) {
-        std::filesystem::path convedPath = path;
-        return convedPath.filename();
-    }
-
-    static std::string getFileExt(std::string path) {
-        std::string ext;
-
-        for(int i = path.length() - 1; i >= 0; i--)
-            if(path[i] == '.') break;
-            else ext = path[i] + ext;
-
-        return ext;
-    }
-
-    static std::string getFullPath(std::string path) {
-        return std::filesystem::absolute(path);
-    }
-
-    static std::string renamePathExt(std::string path, std::string newext) {
-        std::string newpath;
-        std::string oldext = FileManager::getFileExt(path);
-
-        if(path == newext) newpath = path + "." + oldext;
-        else newpath = path.substr(0, path.length() - oldext.length()) + newext;
-        return newpath;
     }
 };
