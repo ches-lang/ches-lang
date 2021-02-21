2f07977d857663b00fe9c399ffaac13a74d6695a
Garnet3106 <manage@gant.work>
2021-02-21 23:21:40 +0900

    add: src > chesc > syntax

---
+++ src/cpp/cmd/chesc/syntax/syntax.cpp
@@ -0,0 +1,111 @@
+/*
+ * Chestnut 0.0.0
+ *
+ * GitHub Repository:
+ *     https://github.com/Garnet3106/chestnut/
+ *
+ * Copyright © 2020-2021 Garnet3106 All rights reserved.
+ */
+
+
+#pragma once
+
+
+namespace ches::cmd::chesc {
+    struct SyntaxTreeNode {
+    public:
+        std::string name = "";
+        std::vector<SyntaxTreeNode> nodes;
+        std::vector<std::string> tokens;
+
+        SyntaxTreeNode();
+    };
+
+
+    struct SyntaxTree {
+    public:
+        SyntaxTreeNode root;
+
+        SyntaxTree();
+    };
+
+
+    enum CPEGExpressionType {
+        CPEGExpression_Unknown,
+        CPEGExpression_ID,
+        CPEGExpression_Regex,
+        CPEGExpression_String
+    };
+
+
+    struct CPEGExpression {
+    public:
+        CPEGExpressionType type = CPEGExpression_Unknown;
+        std::string value;
+    };
+
+
+    /*
+     * spec: 連接の並びをもつ
+     */
+    struct CPEGExpressionSequence {
+    public:
+        std::vector<CPEGExpression> exprs;
+
+        CPEGExpressionSequence();
+    };
+
+
+    enum CPEGExpressionSequenceLoopType {
+        CPEGExpressionSequenceLoop_ZeroOrMore,
+        CPEGExpressionSequenceLoop_OneOrMore,
+        CPEGExpressionSequenceLoop_ZeroOrOne,
+    };
+
+
+    enum CPEGExpressionSequenceLookbehindType {
+        CPEGExpressionSequenceLookbehind_PositiveLookbehind,
+        CPEGExpressionSequenceLookbehind_NegativeLookbehind
+    };
+
+
+    /*
+     * spec: 連接グループの並びをもつ; グループ化されてない場合も要素数 1 として使用する
+     */
+    struct CPEGExpressionSequenceGroup {
+    public:
+        std::vector<CPEGExpressionSequence> exprs;
+        CPEGExpressionSequenceLoopType loopType;
+        CPEGExpressionSequenceLookbehindType lookbehindType;
+
+        CPEGExpressionSequenceGroup();
+    };
+
+
+    /*
+     * spec: 選択の並びをもつ
+     */
+    struct CPEGExpressionChoice {
+    public:
+        std::vector<CPEGExpressionSequenceGroup> seqs;
+
+        CPEGExpressionChoice();
+    };
+
+
+    struct CPEGRule {
+    public:
+        std::string name = "";
+        std::vector<CPEGExpressionChoice> exprChoices;
+
+        CPEGRule(std::string name);
+    };
+
+
+    class CPEG {
+    public:
+        std::vector<CPEGRule> rules;
+
+        CPEG();
+    };
+}
---
+++ src/cpp/cmd/chesc/syntax/syntax.hpp
@@ -0,0 +1,19 @@
+/*
+ * Chestnut 0.0.0
+ *
+ * GitHub Repository:
+ *     https://github.com/Garnet3106/chestnut/
+ *
+ * Copyright © 2020-2021 Garnet3106 All rights reserved.
+ */
+
+
+#pragma once
+
+
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include "./syntax.cpp"
